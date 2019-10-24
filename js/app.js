5c7e081696f81dc6d9de056b57b88efb1553a7eb
Emily Landgrebe <emilandgrebe@gmail.com>
2019-10-23 20:16:30 -0500

    ticket type change to VIP when meet-and-greet is entered

--- js/app.js
+++ js/app.js
@@ -8,14 +8,37 @@ const app = new Vue({
     ticketType: 'general',
     referrals: [],
     specialRequests: '',
-    purchaseAgreementSigned: false,
+    purchaseAgreementSigned: false
+  },
+  watch: {
+    specialRequests: function(newRequests, oldRequests) {
+      if(newRequests.toLowerCase().includes('meet and greet')
+      || newRequests.toLowerCase().includes('meet-and-greet')) {
+        this.ticketType = 'vip';
+      }
+    }
   },
   computed: {
-    fullName: function() {
-      if (this.firstName && this.lastName) {
-        return this.firstName + ' ' + this.lastName;
-      } else {
-        return this.firstName || this.lastName;
+    fullName: {
+      get: function() {
+        if (this.firstName && this.lastName) {
+          return this.firstName + ' ' + this.lastName;
+        } else {
+          return this.firstName || this.lastName;
+        }
+      },
+      set: function(newFullName) {
+        const names = newFullName.split(' ');
+
+        if (names.length === 2) {
+          this.firstName = names[0];
+          this.lastName = names[1];
+        }
+        
+        if (names.length <= 1) {
+          this.firstName = names[0] || '';
+          this.lastName = '';
+        }
       }
     },
     ticketDescription: function() {
@@ -29,8 +52,7 @@ const app = new Vue({
         ticketPluralization = 'ticket';
       }
 
-      return this.ticketQuantity + ' ' + 
-      readableTicketType + ' ' + ticketPluralization;
+      return this.ticketQuantity + ' ' + readableTicketType + ' ' + ticketPluralization;
     }
   }
 });
